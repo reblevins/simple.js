7cf220ebaaa613540143d4720bf25f5b619e29db
Rodney Blevins <rodney@blevins.nl>
2020-07-28 17:00:01 -0600

    two way binding

--- simpel.js
+++ simpel.js
@@ -3,97 +3,92 @@ const path = require('path');
 const Handlebars = require("handlebars");
 
 export const init = (data) => {
-    console.log('Simpel!');
-
     var appTemplate = require('./src/App.hbs');
-    console.log(data);
-    var div = document.createElement('div');
-    div.innerHTML = appTemplate(data);
-    document.body.appendChild(div);
+    console.log(appTemplate);
+    var appDiv = document.createElement('div');
+    appDiv.innerHTML = appTemplate(data);
+    document.body.appendChild(appDiv);
+
+    const name = "AppController"
+    function AppController() {
+        for (let key in data) {
+            this[key] = data[key]
+        }
+    }
+
+    // Store controller constructor
+    var controllers = {};
+    controllers[name] = {
+        factory: AppController,
+        instances: []
+    };
+
+    // Look for elements using the controller
+    // var element = document.querySelector('[controller=' + name + ']');
+    // if (!element){
+    //     return;
+    // }
+
+    // Create a new instance and save it
+    var ctrl = new controllers[name].factory();
+    controllers[name].instances.push(ctrl);
+
+    console.log(appDiv.querySelectorAll('*'));
+    var bindings = {};
+    Array.prototype.slice.call(appDiv.querySelectorAll('[model]'))
+    .map(function (element) {
+        var boundValue = element.getAttribute('model');
+
+        if (!bindings[boundValue]) {
+            bindings[boundValue] = {
+                boundValue: boundValue,
+                elements: []
+            }
+        }
+        bindings[boundValue].elements.push(element);
+    });
+
+    // Update DOM element bound when controller property is set
+        var proxy = new Proxy (ctrl, {
+            set: function (target, prop, value, receiver) {
+                var bind = bindings[prop];
+                if (bind) {
+                    bind.elements.forEach(function (element) {
+                        element.value = value;
+                        element.setAttribute('value', value);
+                        element.innerHTML = value;
+                    });
+                }
+                return Reflect.set(target, prop, value);
+            }
+        });
+
+        // Listen for DOM element update to set the controller property
+        Object.keys(bindings).forEach(function (boundValue) {
+            var bind = bindings[boundValue];
+            // console.log(bind);
+            bind.elements.forEach(function (element) {
+                element.addEventListener('input', function (event) {
+                    proxy[bind.boundValue] = event.target.value;
+                    console.log(appTemplate(proxy));
+                });
+            })
+        });
+
+        // Fill proxy with ctrl properties
+        // and return proxy, not the ctrl!
+        Object.assign(proxy, ctrl);
+        return proxy;
 }
-    // var controllers = {};
     // var debug = document.getElementById('debug')
     // var addController = function (name, constructor) {
-    //     // Store controller constructor
-    //     controllers[name] = {
-    //         factory: constructor,
-    //         instances: []
-    //     };
-    //
-    //     // Look for elements using the controller
-    //     var element = document.querySelector('[controller=' + name + ']');
-    //     if (!element){
-    //         return;
-    //     }
-    //
-    //     // Create a new instance and save it
-    //     var ctrl = new controllers[name].factory();
-    //     controllers[name].instances.push(ctrl);
+
     //
     //     // console.log(controllers);
     //     // Get elements bound to properties
-    //     var bindings = {};
-    //     Array.prototype.slice.call(element.querySelectorAll('[model]'))
-    //     .map(function (element) {
-    //         var boundValue = element.getAttribute('model');
+
     //
-    //         if (!bindings[boundValue]) {
-    //             bindings[boundValue] = {
-    //                 boundValue: boundValue,
-    //                 elements: []
-    //             }
-    //         }
-    //         bindings[boundValue].elements.push(element);
-    //     });
     //
-    //     // Update DOM element bound when controller property is set
-    //     var proxy = new Proxy (ctrl, {
-    //         set: function (target, prop, value) {
-    //             var bind = bindings[prop];
-    //             if (bind) {
-    //                 // console.log(value);
-    //                 bind.elements.forEach(function (element) {
-    //                     console.log(element.tagName.toLowerCase());
-    //                     switch (element.tagName.toLowerCase()) {
-    //                         case 'textarea':
-    //                         case 'input':
-    //                             element.value = JSON.stringify(value);
-    //                             element.setAttribute('value', JSON.stringify(value));
-    //                             break;
-    //                         default:
-    //                             switch (Object.prototype.toString.call(value)) {
-    //                                 case '[object Array]':
-    //                                     handleArray(element, value);
-    //                                     break;
-    //                                 case '[object Object]':
-    //                                     break;
-    //                                 default:
-    //                                     console.log('nuttin');
-    //                             }
-    //                             break;
-    //                     }
-    //                 });
-    //             }
-    //             return Reflect.set(target, prop, value);
-    //         }
-    //     });
-    //
-    //     // Listen for DOM element update to set the controller property
-    //     Object.keys(bindings).forEach(function (boundValue) {
-    //         var bind = bindings[boundValue];
-    //         // console.log(bind);
-    //         bind.elements.forEach(function (element) {
-    //             element.addEventListener('input', function (event) {
-    //                 console.log(event);
-    //                 proxy[bind.boundValue] = event.target.value;
-    //             });
-    //         })
-    //     });
-    //
-    //     // Fill proxy with ctrl properties
-    //     // and return proxy, not the ctrl!
-    //     Object.assign(proxy, ctrl);
-    //     return proxy;
     // }
     //
     // // Export framework in window
