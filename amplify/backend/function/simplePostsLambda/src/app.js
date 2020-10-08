38e60c12fcfb177b88b732ca4918be73e35e79d6
Rodney Blevins <rodney@blevins.nl>
2020-10-08 09:00:25 -0600

    api

--- amplify/backend/function/simplePostsLambda/src/app.js
+++ amplify/backend/function/simplePostsLambda/src/app.js
@@ -58,22 +58,18 @@ const convertUrlType = (param, type) => {
  * HTTP Get method for list objects *
  ********************************/
 
-app.get(path + hashKeyPath, function(req, res) {
+app.get(path, function(req, res) {
   var condition = {}
   condition[partitionKeyName] = {
     ComparisonOperator: 'EQ'
   }
 
-  if (userIdPresent && req.apiGateway) {
-    condition[partitionKeyName]['AttributeValueList'] = [req.apiGateway.event.requestContext.identity.cognitoIdentityId || UNAUTH ];
-  } else {
-    try {
-      condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
-    } catch(err) {
-      res.statusCode = 500;
-      res.json({error: 'Wrong column type ' + err});
-    }
-  }
+	try {
+	  condition[partitionKeyName]['AttributeValueList'] = [ convertUrlType(req.params[partitionKeyName], partitionKeyType) ];
+	} catch(err) {
+	  res.statusCode = 500;
+	  res.json({error: 'Wrong column type ' + err});
+	}
 
   let queryParams = {
     TableName: tableName,
--- seed-db.js
+++ seed-db.js
@@ -2,19 +2,16 @@ const fetch = require('node-fetch');
 
 const posts = [
     {
-        id: 'aaa',
         linkName: 'lorem-ipsum',
         title: 'Lorem ipsum',
         content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
     },
     {
-        id: 'aab',
         linkName: 'dolor-sit',
         title: 'Dolor sit',
         content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
     },
     {
-        id: 'aac',
         linkName: 'consectetur-adipisicing',
         title: 'Consectetur adipisicing',
         content: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
@@ -22,7 +19,7 @@ const posts = [
 ];
 
 posts.forEach(async (post, i) => {
-    const response = await fetch('https://oh1j84ptq6.execute-api.us-east-2.amazonaws.com/dev/post', {
+    const response = await fetch('https://unfspj5l5h.execute-api.us-east-2.amazonaws.com/dev/posts', {
         method: 'POST', // *GET, POST, PUT, DELETE, etc.
         // mode: 'cors', // no-cors, *cors, same-origin
         // cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
